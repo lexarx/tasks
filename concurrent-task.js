1f06e7edbe670d130efa3ecc6f47353cb767a50f
Alexey Nichiporchik <lexarx@gmail.com>
2015-12-24 15:29:49 +0300

    Initial commit

---
+++ concurrent-task.js
@@ -0,0 +1,111 @@
+define('tasks/sequential-task', [
+	'tasks/task', 'collections/observable-collection'
+], function(Task, ObservableCollection) {
+	return Task.extend({
+		/**
+		 * @constructor
+		 * @param {Tasks.ConcurrentTask.Parameters} [parameters]
+		 */
+		constructor: function(parameters) {
+			if (parameters !== undefined && parameters.result !== undefined) {
+				this.returnResult = parameters.result;
+			} else {
+				this.returnResult = false;
+			}
+			this.tasks = new ObservableCollection();
+			this.tasks.changed.add(this.onTasksChanged, this);
+			if (parameters !== undefined && parameters.tasks !== undefined) {
+				this.tasks.setItems(parameters.tasks);
+			}
+			this.super();
+		},
+
+		/**
+		 * @protected
+		 * @param {Collections.ObservableCollection} collection
+		 * @param {Collections.CollectionChange} change
+		 */
+		onTasksChanged: function(collection, change) {
+			if (this.state === Task.State.RUNNING) {
+				throw new Error('Collection of tasks can\'t be modified while the task is running.');
+			}
+			for (var i = 0; i < change.oldItems.length; i++) {
+				var task = change.oldItems[i];
+				task.completed.remove(this.onTaskCompleted, this);
+				task.failed.remove(this.onTaskFailed, this);
+			}
+			for (var i = 0; i < change.newItems.length; i++) {
+				var task = change.newItems[i];
+				task.completed.add(this.onTaskCompleted, this);
+				task.failed.add(this.onTaskFailed, this);
+			}
+		},
+
+		/**
+		 * @protected
+		 * @override
+		 */
+		execute: function() {
+			this.tasksCompleted = 0;
+			this.tasksFailed = 0;
+			if (this.tasks.count() > 0) {
+				this.tasks.each(function(task) {
+					task.run();
+				});
+			} else {
+				this.checkState();
+			}
+		},
+
+		/**
+		 * @protected
+		 */
+		onTaskCompleted: function() {
+			if (this.state !== Task.State.RUNNING) {
+				return;
+			}
+			this.tasksCompleted++;
+			this.checkState();
+		},
+
+		/**
+		 * @protected
+		 * @param {Task} task
+		 * @param {*} error
+		 */
+		onTaskFailed: function(task, error) {
+			if (this.state !== Task.State.RUNNING) {
+				return;
+			}
+			this.error = error;
+			this.tasksFailed++;
+			this.checkState();
+		},
+
+		/**
+		 * @private
+		 */
+		checkState: function() {
+			if (this.tasksCompleted + this.tasksFailed === this.tasks.count()) {
+				if (this.tasksFailed === 0) {
+					var result;
+					if (this.returnResult) {
+						result = this.tasks.map(function(task) {
+							return task.getResult();
+						});
+					}
+					this.complete(result);
+				} else {
+					this.fail();
+				}
+			}
+		}
+	});
+
+	/**
+	 * @typedef Parameters
+	 * @namespace Tasks.ConcurrentTask
+	 * @property {Boolean} result
+	 * @property {Array<Tasks.Task>} tasks
+	 */
+});
---
+++ task.js
@@ -0,0 +1,182 @@
+define('tasks/task', [
+	'class', 'event'
+], function(Class, Event) {
+	var Task = Class.extend({
+		/**
+		 * @constructor
+		 * @param {Function} [callback]
+		 * @param {*} context
+		 */
+		constructor: function(callback, context) {
+			if (callback !== undefined) {
+				this.callback = callback;
+			}
+			if (context !== undefined) {
+				this.context = context;
+			}
+			this.running = new Event();
+			this.completed = new Event();
+			this.failed = new Event();
+			this.state = Task.State.PENDING;
+		},
+
+		/**
+		 * Runs the task
+		 * @param {Function} [onCompleted]
+		 * @param {Function} [onFailed]
+		 * @param {*} [context]
+		 */
+		run: function(onCompleted, onFailed, context) {
+			if (this.state === Task.State.RUNNING) {
+				throw new Error('Task is already running.');
+			}
+			if (onCompleted) {
+				this.completed.add(onCompleted, context);
+			}
+			if (onFailed) {
+				this.failed.add(onFailed, context);
+			}
+			this.state = Task.State.RUNNING;
+			this.result = undefined;
+			this.error = undefined;
+			this.onRunning();
+			this.execute();
+		},
+
+		/**
+		 * @protected
+		 */
+		onRunning: function() {
+			this.running.trigger(this);
+		},
+
+		/**
+		 * @protected
+		 */
+		execute: function() {
+			if (this.callback) {
+				this.callback.call(this.context, this);
+			}
+		},
+
+		/**
+		 * @protected
+		 * @param {*} [result]
+		 */
+		complete: function(result) {
+			if (result !== undefined) {
+				this.result = result;
+			}
+			this.onCompleted();
+		},
+
+		/**
+		 * @protected
+		 */
+		onCompleted: function() {
+			this.completed.trigger(this, this.result);
+		},
+
+		/**
+		 * @protected
+		 * @param {*} [error]
+		 */
+		fail: function(error) {
+			if (error !== undefined) {
+				this.error = error;
+			}
+			this.onFailed();
+		},
+
+		/**
+		 * @protected
+		 */
+		onFailed: function() {
+			this.failed.trigger(this, this.error);
+		},
+
+		/**
+		 * @returns {Tasks.Task.State}
+		 */
+		getState: function() {
+			return this.state;
+		},
+
+		/**
+		 * @returns {Boolean}
+		 */
+		isPending: function() {
+			return this.state === Task.State.PENDING;
+		},
+
+		/**
+		 * @returns {Boolean}
+		 */
+		isRunning: function() {
+			return this.state === Task.State.RUNNING;
+		},
+
+		/**
+		 * @returns {Boolean}
+		 */
+		isCompleted: function() {
+			return this.state === Task.State.COMPLETED;
+		},
+
+		/**
+		 * @returns {Boolean}
+		 */
+		isFailed: function() {
+			return this.state === Task.State.FAILED;
+		},
+
+		/**
+		 * @returns {*}
+		 */
+		getResult: function() {
+			return this.result;
+		},
+
+		/**
+		 * @returns {*}
+		 */
+		getError: function() {
+			return this.error;
+		},
+
+		/**
+		 * @param {Task} task
+		 * @param {Function} onSuccess
+		 */
+		runTask: function(task, onSuccess) {
+			task.run(onSuccess, this.onTaskFailed, this);
+		},
+
+		/**
+		 * @protected
+		 * @param {Task} task
+		 * @param {*} error
+		 */
+		onTaskFailed: function(task, error) {
+			this.fail(error);
+		},
+
+		/**
+		 * @protected
+		 * @param {Task} task
+		 * @param {*} result
+		 */
+		onTaskCompleted: function(task, result) {
+			this.complete(result);
+		}
+	}, {
+		State: {
+			PENDING: 0,
+			RUNNING: 1,
+			COMPLETED: 2,
+			FAILED: 3
+		}
+	});
+
+	return Task;
+});
